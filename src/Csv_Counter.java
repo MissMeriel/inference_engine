8ac736252ab3af0574118a76d4522a22b75afeb3
meriel <meriel.k.stein@gmail.com>
2019-07-01 16:13:37 -0400

    Scripts to deal with csv files

---
+++ src/Csv_Counter.java
@@ -0,0 +1,195 @@
+import java.io.FileInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Arrays;
+import static java.lang.System.out;
+
+public class Csv_Counter{
+
+   static String[][] csv_array;
+   static HashMap<String, HashMap<String,Double>> count_map = new HashMap<String, HashMap<String, Double>>();
+
+   public static void main(String[] args) throws IOException{
+      out.println(Arrays.toString(args));
+      String csv_file = args[0]; 
+      try{
+            csv_array = parse_csv_file(csv_file, get_csv_dimensions(csv_file));
+         } catch(IOException e){
+            e.printStackTrace();
+         }
+      //ArrayList<String> columns = new ArrayList<String>(csv_array[0]);
+      out.println(csv_array[0]);
+      out.println(Arrays.toString(csv_array[0]));
+      ArrayList<String> columns = new ArrayList<String>(Arrays.asList(csv_array[0]));
+      int negative_count = 0;
+      int positive_count = 0;
+      int zero_count = 0;
+      
+      initialize_hashmap(columns);
+      int[] dims = get_csv_dimensions(csv_file);
+      for(int i = 1; i < dims[0]; i++){
+         out.print(i+" ");
+         for(String var_name : columns){
+            int index = columns.indexOf(var_name);
+            if(var_name.equals("TrustDelta")){
+               //out.println(csv_array[i][index]);
+               if(Double.parseDouble(csv_array[i][index]) < 0){
+                  try{
+                     double d = count_map.get(var_name).get("negative_count");
+                     count_map.get(var_name).put("negative_count", ++d);
+                  } catch(NullPointerException ex){
+                     count_map.get(var_name).put("negative_count", 1.0);
+                  }
+                  negative_count++;
+               } else if(Double.parseDouble(csv_array[i][index]) > 0){
+                  try{
+                     double d = count_map.get(var_name).get("positive_count");
+                     count_map.get(var_name).put("positive_count", ++d);
+                  } catch(NullPointerException ex){
+                     count_map.get(var_name).put("positive_count", 1.0);
+                  }
+                  negative_count++;
+               } else {
+                  try{
+                     double d = count_map.get(var_name).get("zero_count");
+                     count_map.get(var_name).put("zero_count", ++d);
+                  } catch(NullPointerException ex){
+                     count_map.get(var_name).put("zero_count", 1.0);
+                  }
+                  zero_count++;
+               }
+            } else if(var_name.equals("Speed_Machine")){
+               //Speed_Machine<5,Speed_Machine>=5&&Speed_Machine<10,Speed_Machine>=10&&Speed_Machine<15,Speed_Machine>=15
+               if(Double.parseDouble(csv_array[i][index]) < 5.0){
+                  try{
+                     double d = count_map.get(var_name).get("Speed_Machine<5");
+                     count_map.get(var_name).put("Speed_Machine<5", ++d);
+                  } catch(NullPointerException ex){
+                     count_map.get(var_name).put("Speed_Machine<5", 1.0);
+                  }
+               } else if(Double.parseDouble(csv_array[i][index]) >= 5.0 && Double.parseDouble(csv_array[i][index]) < 10.0){
+                  try{
+                     double d = count_map.get(var_name).get("Speed_Machine>=5&&Speed_Machine<10");
+                     count_map.get(var_name).put("Speed_Machine>=5&&Speed_Machine<10", ++d);
+                  } catch(NullPointerException ex){
+                     count_map.get(var_name).put("Speed_Machine>=5&&Speed_Machine<10", 1.0);
+                  }
+               } else if (Double.parseDouble(csv_array[i][index]) >= 10.0 && Double.parseDouble(csv_array[i][index]) < 15.0){
+                  try{
+                     double d = count_map.get(var_name).get("Speed_Machine>=10&&Speed_Machine<15");
+                     count_map.get(var_name).put("Speed_Machine>=10&&Speed_Machine<15", ++d);
+                  } catch(NullPointerException ex){
+                     count_map.get(var_name).put("Speed_Machine>=10&&Speed_Machine<15", 1.0);
+                  }
+               } else {
+                  try{
+                     double d = count_map.get(var_name).get("Speed_Machine>=15");
+                     count_map.get(var_name).put("Speed_Machine>=15", ++d);
+                  } catch(NullPointerException ex){
+                     count_map.get(var_name).put("Speed_Machine>=15", 1.0);
+                  }
+               }
+            } else if(var_name.equals("CurrentWheel_MachineDelta")){
+               //CurrentWheel_Machine,DOUBLEDELTA,delta:=10,CurrentWheel_Machine<2&&CurrentWheel_Machine>-2,CurrentWheel_Machine>=2||CurrentWheel_Machine<=-2
+               //CurrentWheel_Machine,DOUBLEDELTA,delta:=10,CurrentWheel_Machine<2.5&&CurrentWheel_Machine>-2.5,CurrentWheel_Machine>=2.5||CurrentWheel_Machine<=-2.5
+               //CurrentWheel_Machine,DOUBLEDELTA,delta:=10,CurrentWheel_Machine<5&&CurrentWheel_Machine>-5,CurrentWheel_Machine>=5||CurrentWheel_Machine<=-5
+               double dd = Double.parseDouble(csv_array[i][index]);
+               if(Double.parseDouble(csv_array[i][index]) < 2.5 && Double.parseDouble(csv_array[i][index]) > -2.5){
+                  try{
+                     double d = count_map.get(var_name).get("CurrentWheel_Machine<2.5&&CurrentWheel_Machine>-2.5");
+                     count_map.get(var_name).put("CurrentWheel_Machine<2.5&&CurrentWheel_Machine>-2.5", ++d);
+                  } catch(NullPointerException ex){
+                     count_map.get(var_name).put("CurrentWheel_Machine<2.5&&CurrentWheel_Machine>-2.5", 1.0);
+                  }
+               } else if(dd >= 2.5 || dd <= 2.5){
+                  try{
+                     double d = count_map.get(var_name).get("CurrentWheel_Machine>=2.5||CurrentWheel_Machine<=-2.5");
+                     count_map.get(var_name).put("CurrentWheel_Machine>=2.5||CurrentWheel_Machine<=-2.5", ++d);
+                  } catch(NullPointerException ex){
+                     count_map.get(var_name).put("CurrentWheel_Machine>=2.5||CurrentWheel_Machine<=-2.5", 1.0);
+                  }
+               }
+            } else {
+               try{
+                  double d = count_map.get(var_name).get(csv_array[i][index]);
+                  count_map.get(var_name).put(csv_array[i][index], ++d);
+               } catch(NullPointerException ex){
+                  count_map.get(var_name).put(csv_array[i][index], 1.0);
+               }
+            }
+         }
+      }
+      out.format("positive_count: %s%nnegative_count: %s%nzero_count: %s%n", positive_count, negative_count, zero_count);
+      out.println(count_map);
+      print_count_map();
+      print_percentages(dims[0]-1);
+   }
+   
+   public static void initialize_hashmap(ArrayList<String> columns){
+      for (String s : columns){
+         count_map.put(s, new HashMap<String,Double>());
+      }
+   }
+   
+   public static void print_count_map(){
+      out.println("COUNT MAP:");
+      Set<String> keys1 = count_map.keySet();
+      for(String key1 : keys1){
+         Set<String> keys2 = count_map.get(key1).keySet();
+         for (String key2 : keys2){
+            out.println(key1+":"+key2+":=" +count_map.get(key1).get(key2));
+         }
+      }
+   }
+   
+   
+   public static void print_percentages(int i){
+      out.println("\n\n\n\n\n\n\n\n\nPERCENTAGES:");
+      Set<String> keys1 = count_map.keySet();
+      for(String key1 : keys1){
+         if(!key1.contains("GSR") || !key1.contains("Manual")  || !key1.contains("Auto")  || !key1.contains("Pupil") ){
+            Set<String> keys2 = count_map.get(key1).keySet();
+            for (String key2 : keys2){
+               out.format("%s:%s:=%.3f%n",key1,key2,count_map.get(key1).get(key2)/i);
+               //out.println(key1+":"+key2+"=" +count_map.get(key1).get(key2));
+            }
+         }
+      }
+   }
+   
+   
+   public static int[] get_csv_dimensions(String csv_file) throws IOException {
+      FileReader fis = new FileReader(csv_file);
+      BufferedReader dis = new BufferedReader(fis);
+      String thisLine;
+      // rows x columns
+      int[] dims = {0, 0};
+      while((thisLine = dis.readLine()) != null){
+         dims[0] += 1;
+         dims[1] = (thisLine.split(",").length);
+      }
+      return dims;
+   }
+   
+   public static String[][] parse_csv_file(String csv_file, int[] dims) throws IOException {
+      FileInputStream fis = new FileInputStream(csv_file);
+      DataInputStream dis = new DataInputStream(fis);
+      String thisLine;
+      // rows x columns
+      String[][] csv_array = new String [dims[0]][dims[1]];
+      int row_count = 0;
+      while ((thisLine = dis.readLine()) != null){
+         String[] cols = thisLine.split(",");
+         csv_array[row_count]= cols;
+         row_count++;
+      }
+      fis.close();
+      dis.close();
+      return csv_array;
+   }
+}
---
+++ src/Probability_To_Csv.java
@@ -0,0 +1,165 @@
+import java.io.FileInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.io.FileWriter;
+import static java.lang.System.out;
+
+class Probability_To_Csv{
+
+   //static String[][] csv_array;
+   static HashMap<String, HashMap<String,String>> header_map = new HashMap<String, HashMap<String, String>>();
+   static Pattern subject_pattern = Pattern.compile("[S][0-9]+[\\s]*");
+   static Pattern whitespace_pattern = Pattern.compile("^\\S+");
+   static Pattern double_pattern = Pattern.compile("[0-9]+.[0-9]+");
+   //static Pattern whitespace_pattern = Pattern.compile("\p{IsWhite_Space}+");
+   static ArrayList<String> headers = new ArrayList<String>();
+   
+   public static void main(String[] args) throws IOException{
+      out.println(Arrays.toString(args));
+      String filename = args[0]; 
+      try{
+            parse_file(filename, get_length(filename));
+         } catch(IOException e){
+            e.printStackTrace();
+         }
+      //ArrayList<String> columns = new ArrayList<String>(csv_array[0]);
+      //out.println(csv_array[0]);
+      //out.println(Arrays.toString(csv_array[0]));
+      print_header_map();
+      String csv_string = map_to_csv();
+      String csv_filename = filename.replace("txt","csv");
+      out.println(csv_filename);
+      write_csv_to_file(csv_filename, csv_string);
+   }
+   
+   public static void write_csv_to_file(String filename, String contents){
+      try{
+         FileWriter fw = new FileWriter(filename);
+         fw.write(contents);
+         fw.close();
+      } catch(Exception e){
+         e.printStackTrace();
+      }
+      out.format("Csv written to %s%n",filename);
+   }
+   
+   public static void parse_file(String csv_file, int length) throws IOException {
+      FileInputStream fis = new FileInputStream(csv_file);
+      DataInputStream dis = new DataInputStream(fis);
+      String thisLine;
+      // rows x columns
+      String[] line_array = new String [length];
+      int row_count = 0;
+      String current_subject = "";
+      while ((thisLine = dis.readLine()) != null){
+         Matcher subject_matcher = subject_pattern.matcher(thisLine);
+         Matcher whitespace_matcher = whitespace_pattern.matcher(thisLine);
+         if(subject_matcher.matches()){
+            out.println(thisLine);
+            current_subject = thisLine;
+            //continue;
+         } else if(whitespace_matcher.find()){
+            out.format("Parsing %s line %s%n", current_subject, thisLine);
+            parse_line(current_subject, thisLine);
+            //csv_array[row_count]= cols;
+            out.println();
+         }
+         row_count++;
+      }
+      fis.close();
+      dis.close();
+      //return line_array;
+   }
+   
+   public static void parse_line(String subject, String line) {
+      String[] cols = line.split("\\) ="); //length==2
+      HashMap<String,String> subject_map = new HashMap<String,String>();
+      
+      try{
+         HashMap<String,String> temp = header_map.get(subject);
+         subject_map = temp;
+         if(temp == null){
+            subject_map = new HashMap<String,String>();
+            header_map.put(subject, subject_map);
+         }
+      } catch(NullPointerException ex) {
+         ex.printStackTrace();
+         subject_map = new HashMap<String,String>();
+         header_map.put(subject, subject_map);
+      }
+      String header = String.format("%s)",cols[0]);
+      String[] temp = cols[1].split(" = ");
+      String probability = temp[temp.length-1];
+      out.format("subject_map.put(%s, %s)%n",header,probability);
+      out.format("subject_map == null? %s%n",(subject_map == null));
+      if(!headers.contains(header)){
+         headers.add(header);
+      }
+      subject_map.put(header, probability);
+      header_map.put(subject, subject_map);
+   }
+   
+   public static void print_header_map(){
+      out.println("\n\n\nHEADER MAP:");
+      Set<String> keys1 = header_map.keySet();
+      TreeSet<String> treeset1 = new TreeSet(keys1);
+      for(String key1 : treeset1){
+         Set<String> keys2 = header_map.get(key1).keySet();
+         TreeSet<String> treeset2 = new TreeSet(keys2);
+         for (String key2 : treeset2){
+         
+            out.println(key1+":"+key2+":=" +header_map.get(key1).get(key2));
+         }
+      }
+      out.println();
+      out.println(headers);
+   }
+   
+   public static String map_to_csv(){
+      out.println("\n\n\nmap_to_csv()");
+      String headers_str = headers.toString();
+      String csv_string = "Subject,"+headers_str.substring(1, headers_str.length()-1);
+      Set<String> keys1 = header_map.keySet();
+      TreeSet<String> treeset1 = new TreeSet(keys1);
+      for(String key1 : treeset1){
+         Set<String> keys2 = header_map.get(key1).keySet();
+         TreeSet<String> treeset2 = new TreeSet(keys2);
+         csv_string += String.format("%n%s",key1);
+         for(String header : headers){
+            String temp = header_map.get(key1).get(header);
+            if(temp == null){
+               out.format("header_map.get(%s).get(%s) == null%n",key1,header);
+               csv_string += ",0.0";
+            } else {
+               csv_string += "," +temp;
+            }
+            
+         }
+      }
+      out.println("\n\n\n\n");
+      out.println(csv_string);
+      return csv_string;
+   }
+
+   public static int get_length(String file) throws IOException {
+      FileReader fis = new FileReader(file);
+      BufferedReader dis = new BufferedReader(fis);
+      String thisLine;
+      // rows x columns
+      int len = 0;
+      while((thisLine = dis.readLine()) != null){
+         len += 1;
+      }
+      return len;
+   }
+
+}
